7edab22c25251daef4e77bf1d8faa263e5622d6a
何东升 <heds@signalway.com.cn>
2022-08-23 17:35:39 +0800

    feat(app): 多语言 i18n

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import { createApp } from 'vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
-import { createPinia } from 'pinia'
+import {createApp} from 'vue'
+import {createRouter, createWebHashHistory} from 'vue-router'
+import {createPinia} from 'pinia'
+import {createI18n} from 'vue-i18n'
+import messagesaaa from './i18n/i18n'
 
 import App from './App.vue'
-
 
 
 import pageRoutes from '~pages'
@@ -12,7 +13,7 @@ console.log('pageRoutes');
 console.log(pageRoutes);
 
 const routes = [
-    { path: '/', redirect: '/dashboard' }
+    {path: '/', redirect: '/dashboard'}
 ];
 
 const router = createRouter({
@@ -25,8 +26,16 @@ const router = createRouter({
     ]
 })
 
+// 通过选项创建 VueI18n 实例
+const i18n = createI18n({
+    locale: 'en', // 设置地区
+    messages: messagesaaa, // 设置地区信息
+})
+
+
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
+app.use(i18n)
 app.mount('#app')
 
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
         login page
+        <div>{{$t("i18n.tips")}}</div>
     </div>
 </template>
